eea0f3d24a6ac32758f24fe2b554f9e3f54731dc
Pablo Misael Peloc <pmpeloc@gmail.com>
2022-01-31 20:53:23 -0300

    feat: Task Basic finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTasks([...tasks, task]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Task Basic</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type='text'
+          placeholder='Ingrese la tarea'
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type='submit'>Agregar Tarea</button>
+      </form>
+      <div className='tasks-container'>
+        {tasks.map((item, index) => (
+          <div key={index} className='task-card'>
+            {item}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
